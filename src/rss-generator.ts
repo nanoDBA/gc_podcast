8f9ac6c66383e3ba8902fad9cb26e35c22a4c10d
Lars Rasmussen <larsra@outlook.com>
2026-01-08 10:31:26 -0500

    Use better conference artwork (1400px hero image)

--- src/rss-generator.ts
+++ src/rss-generator.ts
@@ -17,7 +17,7 @@ const PODCAST_CONFIG = {
   category: 'Religion & Spirituality',
   subcategory: 'Christianity',
   explicit: false,
-  imageUrl: 'https://www.churchofjesuschrist.org/imgs/nivn2mb3of0ew2b7qthi5ra6etm2pupessmvzvqs/full/!500,/0/default',
+  imageUrl: 'https://www.churchofjesuschrist.org/imgs/5uahv05h1s6416y49vw745z70juiiffhiq0vn8a2/full/!1400,/0/default',
   websiteUrl: 'https://www.churchofjesuschrist.org/study/general-conference',
   copyright: `© ${new Date().getFullYear()} by Intellectual Reserve, Inc. All rights reserved.`,
 };
